f437ccf53621ffb63c8560fc0d511ddab9712f36
Dominik Kupka <dominik.kupka@allgeier-engineering.com>
2019-01-31 16:47:25 +0100

    same state as master but the php path for the mail sender in registerpage.php is for local tests and not for server and the db settings are for local pc and not for server

--- scripts/LoginWithKey.php
+++ scripts/LoginWithKey.php
@@ -86,6 +86,7 @@ if(isset($_GET['login'])) {
 				<section class="wrapper style1">
 					<div class="container">
 						<div id="content">
+							<h2 style="color: green">Die Registrierung hat funktioniert.<br/> Wir haben Ihnen eine E-Mail mit einem Lizenzschlüssel zugesendet.</h2>
 
 							<!-- Content -->
 							<form action="?login=1" method="post">
