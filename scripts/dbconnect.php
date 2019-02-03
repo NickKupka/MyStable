d0107bb3595298866237ef3b49e7652b62f782c8
Dominik Kupka <dominik.kupka@allgeier-engineering.com>
2019-02-03 12:52:53 +0100

    dbconnect.php hatte noch einen bug mit der db connection - behoben - registrierung auf der live seite funktioniert.

--- scripts/dbconnect.php
+++ scripts/dbconnect.php
@@ -1,10 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $servername="localhost";
-//$benutzername="MyStableDBRoot";
-//$passwort="Nick&Alex2019";
-$benutzername="root";
-$passwort="";
+$benutzername="MyStableDBRoot";
+$passwort="Nick&Alex2019";
 
 $dbname="mystable";
 $db = new mysqli($servername, $benutzername, $passwort, $dbname) or die ("Verbindungsfehler: " . $db->error);
